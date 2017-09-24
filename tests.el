d9dbb83e0b87106901a120692286bd1f021a392d
pfeodrippe <pfeodrippe@gmail.com>
2017-09-24 16:11:48 -0300

    first commit

---
+++ tests.el
@@ -0,0 +1,39 @@
+;;; package -- Summary
+;;; Commentary:
+
+(require 'cider)
+(require 'buttercup)
+(require 'cider-interaction)
+(require 'cider-browse-ns)
+(require 'clomacs)
+
+
+;;; Code:
+(defun eita-test ()
+  (interactive)
+  ;; (with-current-buffer "*cider-repl banka*"
+  ;;   (clojure-mode)
+  ;;   ;;(save-excursion (insert "(defn a [] (let [x 1] (inc x)) {:a 1, :b 2})"))
+  ;;   (print (cider--var-namespace "#'c/var-two"))
+  ;;   ;;(cider--debug-move-point '(3 2 1))
+  ;;   )
+
+  ;;(cider-insert-in-repl "(+ 1 2)" t)
+  ;;(cider-insert-region-in-repl 1 20)
+
+  ;;(print (cider-defun-at-point))
+  (print (point))
+  ;;(cider--pprint-eval-form "(+ 1 4)")
+  ;;(cider-interactive-eval "(+ 1 77)")
+
+  (print (replace-regexp-in-string "defn"
+                                   "defeita"
+                                   (cider-defun-at-point)))
+
+  (clomacs-defun summ-1 +)
+  (print (summ-1 2 22))
+  )
+
+(global-set-key (kbd "C-c C-a tt") 'eita-test)
+
+;;; tests.el ends here
